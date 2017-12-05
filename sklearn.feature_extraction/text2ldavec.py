29b89bfc034c98bb36aad34fb76810fea647d2e7
liu-nlper <jxliu.nlper@gmail.com>
2017-12-05 23:24:19 +0800

    update

--- sklearn.feature_extraction/text2ldavec.py
+++ sklearn.feature_extraction/text2ldavec.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 """
-    用tf-idf表示文档，TfidfTransformer类
+    用lda表示文档，CountVectorizer类
 """
 from nltk.tokenize import word_tokenize
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.decomposition import LatentDirichletAllocation
 
 documents = ["Human machine interface for lab abc computer applications",
              "A survey of user opinion of computer system response time",
@@ -22,18 +22,16 @@ stop_words = ['a', 'of', 'for']
 count_vec = CountVectorizer(tokenizer=word_tokenize, stop_words=stop_words)
 sparse_matrix_count = count_vec.fit_transform(documents)
 
-tfidf_vec = TfidfTransformer()
-tfidf_vec.fit(sparse_matrix_count)
-tfidf_vec.transform(sparse_matrix_count)
+# lda model
+num_topics = 10
+lda_vec = LatentDirichletAllocation(n_components=num_topics)
+lda_vec.fit(sparse_matrix_count)
 
-sparse_matrix_tfidf = tfidf_vec.transform(sparse_matrix_count)
-print(sparse_matrix_tfidf)
+sparse_matrix_lda = lda_vec.transform(sparse_matrix_count)
 
-# 将一个新的文档映射到tfidf空间
+# 将一个新的文档映射到lda空间
 new_doc = "The generation of random xxx"
 new_doc_count = count_vec.transform([new_doc])
-new_doc_tfidf = tfidf_vec.transform(new_doc_count)
-print(new_doc_tfidf)
-
-# 转为稠密矩阵
-new_doc_tfidf.toarray()
+new_doc_lda = lda_vec.transform(new_doc_count)
+print(new_doc_lda)  # np.array
+print(new_doc_lda.shape)  # (1, 10)
