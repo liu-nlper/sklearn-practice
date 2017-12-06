1c53f026903a3fff31d36b1ed08f1afe843d4bd7
liu-nlper <jxliu.nlper@gmail.com>
2017-12-06 22:12:20 +0800

    update

--- sklearn.classification/README.md
+++ sklearn.classification/README.md
@@ -14,6 +14,20 @@
 
 ### 3. KNN
 
-[`knn.py`](https://github.com/liu-nlper/sklearn-practice/blob/master/sklearn.classification/knn.py)，利用`knn`算法进行文本分类。
+[`knn.py`](https://github.com/liu-nlper/sklearn-practice/blob/master/sklearn.classification/knn.py)，利用`knn`算法进行文本分类，参考文档：[`sklearn文档`](http://scikit-learn.org/stable/modules/generated/sklearn.neighbors.KNeighborsClassifier.html#sklearn.neighbors.KNeighborsClassifier)
 
 可选用的算法包括：`ball_tree`、`kd_tree`和`brute`，该算法同样可用于相似`文档检索`任务。
+
+### 4. Naive Bayes
+
+[`naive_bayes.py`](https://github.com/liu-nlper/sklearn-practice/blob/master/sklearn.classification/naive_bayes.py)，朴素贝叶斯算法在文本分类任务上的应用，参考文档：[`sklearn文档`](http://scikit-learn.org/stable/modules/generated/sklearn.naive_bayes.GaussianNB.html#sklearn.naive_bayes.GaussianNB)
+
+#### 5. Ensemble methods
+
+集成算法在文本分类上的应用。
+
+##### 5.1 Random Forests
+
+[`random_forests.py`](https://github.com/liu-nlper/sklearn-practice/blob/master/sklearn.classification/random_forests.py)，参考文档：[`sklearn文档`](http://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestClassifier.html#sklearn.ensemble.RandomForestClassifier)
+
+**注**：值得注意的是，rf可以对特征的重要程度进行排序，可以用作特征选择。
---
+++ sklearn.classification/random_forests.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+    random forests算法在文本分类任务上的应用
+"""
+
+import numpy as np
+from nltk.tokenize import word_tokenize
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+
+
+# 加载数据集。四个类别共3387个实例
+print('加载数据集...')
+categories = [
+    'alt.atheism',
+    'talk.religion.misc',
+    'comp.graphics',
+    'sci.space',
+]
+dataset = fetch_20newsgroups(subset='all', categories=categories, shuffle=True, random_state=42)
+X_ = dataset.data
+y = dataset.target
+# 类别数
+nb_classes = np.unique(y).shape[0]
+
+# 将训练数据映射到tf-idf空间
+print('将数据映射到tf-idf向量空间...')
+pipeline = Pipeline(steps=[
+    ('vect', CountVectorizer(stop_words='english', tokenizer=word_tokenize, max_df=0.6, min_df=0.01)),
+    ('tfidf', TfidfTransformer()),
+])
+X = pipeline.fit_transform(X_).toarray()
+
+# 划分训练集、测试集
+print('划分训练集和测试集...')
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, random_state=42, stratify=y, test_size=0.2)
+
+# 建立模型
+rnf_model = RandomForestClassifier(n_estimators=200, max_depth=20)
+
+# 训练
+print('训练模型...')
+rnf_model.fit(X_train, y_train)
+
+# 预测
+print('预测...')
+y_pred_train = rnf_model.predict(X_train)
+y_pred_test = rnf_model.predict(X_test)
+
+# 计算acc
+acc_train = np.sum(y_train == y_pred_train) / y_train.shape[0]
+acc_test = np.sum(y_test == y_pred_test) / y_test.shape[0]
+
+print('acc of train: %f' % acc_train)  # 0.960133
+print('acc of test: %f' % acc_test)  # 0.890855
+
+# 值得注意的是，rf可以对特征的重要程度进行排序，可以用作特征选择
+print(len(rnf_model.feature_importances_))
+print(rnf_model.feature_importances_)
